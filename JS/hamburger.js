009ac3c17365541de78ccb3b66b2758014ee1203
rand <randossa33@gmail.com>
2025-12-01 17:40:36 +0200

    convert hamburger to jquery

--- JS/hamburger.js
+++ JS/hamburger.js
@@ -1,9 +1,5 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let hamburger = document.getElementById("hamburger");
-  let navDesc = document.getElementById('navLinks');    // links container
-  if (hamburger) {
-    hamburger.addEventListener("click", function () {
-      navDesc.classList.toggle('open');
-    });
-  }
+$(document).ready(function () {
+  $("#hamburger").on("click", function () {
+    $("#navLinks").toggleClass("open");
+  });
 });
